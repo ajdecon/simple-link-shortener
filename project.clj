2d1fc31ac8f08dd712078e36c82b39ad4eb7c56d
Adam DeConinck <ajdecon@ajdecon.org>
2018-04-29 17:20:15 -0700

    Simple API definition, no implementation yet

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/java.jdbc "0.7.6"]
+                 [org.xerial/sqlite-jdbc "3.21.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler simple-link-shortener.handler/app}
   :profiles
--- src/simple_link_shortener/handler.clj
+++ src/simple_link_shortener/handler.clj
@@ -1,11 +1,54 @@
 (ns simple-link-shortener.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.util.response :as response]
+            [ring.middleware.json :as middleware]
+            [clojure.java.jdbc :refer :all :as jdbc]))
 
+; Local SQLite database
+(def db
+  {:classname "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname "db/urls.db"})
+
+; Make a generic error response
+(defn make-error [text]
+  (response/content-type
+    (response/status
+      (response/response {:error text})
+      301)
+    "application/json"
+  ))
+
+; Make a generic response
+(defn make-response [text]
+  (response/content-type (response/response {:text text}) "application/json"))
+
+;(defn get-all-urls [] (make-error "not implementend"))
+(defn get-all-urls [] (make-response db))
+
+(defn add-url [request] (make-error "not implemented"))
+
+(defn redirect-to [id] (make-error "not implemented"))
+
+(defn remove-url [id] (make-error "not implemented"))
+
+; Application routes
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (context "/urls" []
+    (defroutes urls-routes
+      (GET "/" [] (get-all-urls))
+      (POST "/" request (add-url request))
+      (context "/:id" [id]
+        (GET "/" [] (redirect-to id))
+        (DELETE "/" [] (remove-url id)))))
   (route/not-found "Not Found"))
 
+; Define the app to actually run!
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (->
+    (wrap-defaults app-routes api-defaults)
+    ; JSON-ify bodies and responses
+    (middleware/wrap-json-body)
+    (middleware/wrap-json-response)))
